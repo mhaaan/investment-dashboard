000d1991d6fd24bef070451cef5d058b1c16d4b6
Michael Han <michael.han@rwe.com>
2025-08-05 18:19:56 +0100

    api set up

---
+++ backend/trading212/handler.py
@@ -0,0 +1,41 @@
+# Create a Lambda function
+# The Lambda will be a simple 'fetch my trading 212 data'
+# We'll connnect this Lambda to API Gateway so that React can call it
+
+# handler.py is the python file containing the Lambda function
+# AWS Lambda will call a specific function inside this file every time its triggered
+# By default, AWS Lambda will look for a function called 'lambda_handler'
+
+import json # Provides a way to encode and decode JSON data, which is commonly used for data interchange in web applications
+import os # Provides way of using operating system dependent functionality, such as accessing environment variables, working with file paths and performing file and directory operationsl
+import requests # Allows you to send HTTP requests, which is useful for interacting with web services and APIs
+
+from dotenv import load_dotenv # Used to load environment variables from a .env file, which is useful for managing configuration settings in a secure way
+
+load_dotenv() # Used to load the environement variable from a .env file (ALWASY ADD .ENV TO .GITIGNORE!!!!!!!)
+
+def lambda_handler(event, context):
+    trading_212_api_key = os.environ.get('212_API_KEY')
+    if not trading_212_api_key:
+        return {
+            'statusCode': 500,
+            'body': 'API key not set!!!'
+        }
+
+    url = "https://live.trading212.com/api/v0/equity/portfolio"
+    headers = {"Authorization": trading_212_api_key}
+
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+        return {
+            "statusCode": 200,
+            "body": json.dumps(data)
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
+
